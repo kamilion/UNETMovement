2e643ce0fed3e9f4e49a16e0cc8f9543361a9a16
Kamilion <kamilion@gmail.com>
2016-01-17 02:18:40 -0800

    Move stuff around in the repo...

---
+++ UNETMovement/Assets/Scripts/Inventory.cs
@@ -0,0 +1,129 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+using System.Collections.Generic;
+
+public struct PickupInfo{
+	public int itemIndex;
+	public int itemAmmo;
+}
+[NetworkSettings(channel=0,sendInterval=0.05f)]
+public class Inventory : NetworkBehaviour {
+    public GameObject m_itemRenderers;
+	public int maxSlotsCount = 6;
+	public int _currentSlot = 0;
+	public ItemScript[] _availableItems;//Array of available items specific to this entity
+
+	public SyncListInt _slots= new SyncListInt ();//List of item index to sync over network
+	
+	[SyncVar]
+	private int _syncSlot;//Selected slot
+	
+	private int _input;
+
+	private int _lastSlot = -1;
+
+	private bool _holster = false;
+	
+	public void GiveItem(PickupInfo itemInfo){
+		if (hasAuthority) {
+			//Create all slots if not created yet
+			if(_slots.Count < maxSlotsCount){
+				for(int i =0;i < maxSlotsCount;i++){
+					_slots.Add(-1);
+				}
+				//if it is a first item switch to it
+				_currentSlot = _availableItems[itemInfo.itemIndex].slot;
+				_syncSlot = _currentSlot;
+				Rpc_FirstItem(_currentSlot);
+			}
+
+			if(_slots[_availableItems[itemInfo.itemIndex].slot] == itemInfo.itemIndex){
+				//Add ammo only if item already is in inventory
+				_availableItems[itemInfo.itemIndex].GiveAmmo(itemInfo.itemAmmo);
+			}else{
+				//Add item and ammo
+				_slots[_availableItems[itemInfo.itemIndex].slot] = itemInfo.itemIndex;
+				_availableItems[itemInfo.itemIndex].GiveAmmo(itemInfo.itemAmmo);
+			}
+		}
+	}
+	//Needed to switch client to first item
+	[ClientRpc]
+	void Rpc_FirstItem(int slot){
+		_currentSlot = slot; 
+	}
+
+	void LateUpdate(){
+		if (isLocalPlayer) {
+			//Get player inputs
+			GetInputs ();
+			//Checking if it's possible to switch to new slot
+			if (_slots.Count > 0 && _input < _slots.Count && _slots [_input] >= 0 && _availableItems [_slots [_input]]) {
+				_currentSlot = _input;
+			}
+			if (hasAuthority) {
+				_syncSlot = _currentSlot;
+			} else {
+				Cmd_SetSlot (_currentSlot);
+			}
+		} else {
+			if(!hasAuthority){
+				_currentSlot = _syncSlot;
+			}
+		}
+		//Actual switching
+		if (_holster) {
+			if (_availableItems [_slots [_currentSlot]].selected){
+				_availableItems [_slots [_currentSlot]].Deselect ();
+			}
+		} else {
+			if ( _slots.Count > 0 && _currentSlot < _slots.Count && _availableItems [_slots [_currentSlot]]) {
+				//Disabling last item
+				if (_lastSlot >= 0 && _lastSlot != _currentSlot) {
+					if (_availableItems [_slots [_lastSlot]].selected) {
+						_availableItems [_slots [_lastSlot]].Deselect ();
+					} else {
+						_lastSlot = _currentSlot;
+					}
+				} else {
+					_lastSlot = _currentSlot;
+				}
+				//Enabling new item
+				if (!_availableItems [_slots [_currentSlot]].selected) {
+					_availableItems [_slots [_currentSlot]].Select ();
+				}
+			}
+		}
+	}
+
+	void GetInputs(){
+		if (Input.GetButtonDown ("Slot1")) {
+			_input = 0;
+		} else if (Input.GetButtonDown ("Slot2")) {
+			_input = 1;
+		} else if (Input.GetButtonDown ("Slot3")) {
+			_input = 2;
+		} else if (Input.GetButtonDown ("Slot4")) {
+			_input = 3;
+		} else if (Input.GetButtonDown ("Slot5")) {
+			_input = 4;
+		} else if (Input.GetButtonDown ("Slot6")) {
+			_input = 5;
+		}
+
+		if (Input.GetButtonDown ("Holster")) {
+			_holster = !_holster;
+		}
+	}
+
+	[Command(channel=0)]
+	void Cmd_SetSlot(int input){
+		if (!isLocalPlayer && hasAuthority) {
+			_currentSlot = input;
+			_syncSlot = _currentSlot;
+		}
+	}
+
+
+}
---
+++ UNETMovement/Assets/Scripts/ItemScript.cs
@@ -0,0 +1,100 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+
+public class ItemScript : MonoBehaviour {
+	public int slot;
+	public bool selected = false;
+    public int m_ammoInGun;
+    public int m_ammoInPocket;
+    public Transform _bullet;
+	public int AnimationType = 0;
+	public WeaponController _weaponController;
+	public Animator animator;
+	public Transform Aimpoint;
+	public Transform ShootPoint;
+    public List<MeshRenderer> m_meshRenderers;
+	public float FireTime = 0.1f;
+	private float _lastFireTime = 0;
+	private Vector3 _startPos;
+	private Quaternion _startRot;
+
+
+
+	public void GiveAmmo(int amount){
+        m_ammoInPocket += amount;
+	}
+
+	public void Select(){
+		if (animator.GetBool ("Holstered")) {
+			selected = true;
+			animator.SetInteger ("AnimationType", AnimationType);
+			animator.SetBool ("Holster", false);
+            ItemExposed (true);
+			//gameObject.SetActive (true);
+		}
+	}
+
+	public void Deselect(){
+		if (animator.GetBool ("Holstered")) {
+			selected = false;
+            ItemExposed (false);
+            //gameObject.SetActive (false);
+		} else {
+			animator.SetInteger ("AnimationType", -1);
+			animator.SetBool ("Holster", true);
+		}
+	}
+
+	// Use this for initialization
+	//void Start () {	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		_startPos = ShootPoint.position;
+		_startRot = ShootPoint.rotation;
+	}
+
+	public bool Fire1(){
+        if (!(m_ammoInGun >= 1)) { // If you don't have ammo
+            return false;
+        } else {  // If you do have ammo
+		    if (Time.time >= (_lastFireTime + FireTime)) {
+			    _lastFireTime = Time.time;
+                return true;
+		    } else {
+			    return false;
+		    }
+        } 
+    }
+
+    public bool Fire2()
+    { // THIS IS ACTUALLY A CHECK TO SEE IF YOU *CAN* RELARD
+        Debug.Log("Your weapon is now reloaded magically.");
+        m_ammoInGun = 20;
+        m_ammoInPocket = (m_ammoInPocket - m_ammoInGun);
+        return true;
+    }
+
+    public void ItemExposed(bool exposed)
+    {
+        foreach (MeshRenderer meshRenderer in m_meshRenderers)
+        {
+            meshRenderer.GetComponent<Renderer>().enabled = exposed;
+        }
+    }
+
+
+    public BulletScript PrepareBullet(){
+		Transform bullet = (Transform)Instantiate (_bullet, _startPos, _startRot);
+		return bullet.GetComponent<BulletScript> ();
+
+	}
+
+	public void Shoot(bool isOwner,byte shotID,BulletScript bullet ){
+		ShootPoint.SendMessage ("Play", SendMessageOptions.DontRequireReceiver);
+        m_ammoInGun--; // Decrement ammo in the gun
+        bullet.Shoot (_weaponController, isOwner,shotID);
+    }
+}
